561766099a94b9d6f13451de7e8320adc9afabc7
Sichkar Yurii <si4kar2017@gmail.com>
2024-01-13 16:21:15 +0200

    users

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 const categories = ['bbqs', 'bests', 'breads', 'burgers', 'chocolates', 'desserts', 'drinks', 'fried-chickens', 'ice-creams', 'pizzas', 'porks', 'sandwiches', 'sausages', 'steaks', 'foods'];
-const URL = 'mongodb+srv://Yurets:1045_Poseydon@cluster0.i5lemqa.mongodb.net/inloftmenu';
 const menuItems = ['tables', 'personnels', 'sales'];
 
-module.exports = {categories, URL, menuItems};
+module.exports = {categories, menuItems};
