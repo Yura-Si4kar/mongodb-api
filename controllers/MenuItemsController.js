4310a08a32abceed69116418a956ad7ae6071275
Sichkar Yurii <si4kar2017@gmail.com>
2024-01-17 19:25:11 +0200

    remove img logic

--- controllers/MenuItemsController.js
+++ controllers/MenuItemsController.js
@@ -1,9 +1,5 @@
-const { v4 } = require('uuid');
-const path = require('path');
-const fs = require('fs');
 const Models = require('../models/model');
 const ApiError = require('../error/ApiError');
-const sharp = require('sharp');
 
 class MenuItemsController {
     constructor(category) {
@@ -12,31 +8,10 @@ class MenuItemsController {
     
     async create(req, res, next) {
         try {
-            if (req.files) {
-                let { img } = req.files;
-                console.log(img);
-                let fileName = v4() + '.jpg';
-    
-                sharp(img.data)
-                .jpeg({ quality: 80 })
-                .toFile(path.resolve(__dirname, '..', 'static', fileName), (err, info) => {
-                    if (err) {
-                        console.error(err);
-                    } else {
-                        console.log(info);
-                    }
-                });
-    
-                const list = new this.Model({ ...req.body, img: fileName });
-                const result = await list.save();
-    
-                res.status(201).json(result);    
-            } else {
-                const list = new this.Model(req.body);
-                const result = await list.save();
-    
-                res.status(201).json(result);    
-            }
+            const list = new this.Model(req.body);
+            const result = await list.save();
+
+            res.status(201).json(result);    
         } catch (error) {
             next(ApiError.internal(error.message));
         }
@@ -51,20 +26,6 @@ class MenuItemsController {
             }
 
             res.status(200).json(list);
-        } catch (error) {
-            next(ApiError.internal(error.message));
-        }
-    }
-
-    async getOne(req, res, next) {
-        try {
-            const doc = await this.Model.findById(req.params.id);
-
-            if (!doc) {
-                return next(ApiError.notFound('Element not found'));
-            }
-
-            res.status(200).json(doc);
         } catch (error) {
             next(ApiError.internal(error.message));
         }
